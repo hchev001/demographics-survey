0221825b21c58b55f3c34adbb3d892fbe5b5d2d5
Hamilton Chevez <hchev001@fiu.edu>
2018-09-23 20:14:55 -0400

    set up basic boilerplate for the backend

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import config from "config";
+
+let app = express();
+
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(
+//     config.DBHost,
+//     {
+//       connectTimeoutMS: 30000,
+//       keepAlive: 1,
+//       useNewUrlParser: true
+//     }
+//   )
+//   .catch(() => {
+//     console.log("Failed to connect to mongodb server");
+//   });
+let port = config.PORT;
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    config.DBHost,
+    {
+      connectTimeoutMS: 30000,
+      keepAlive: 1,
+      useNewUrlParser: true
+    }
+  )
+  .catch(() => {
+    console.log("Failed to connect to mongodb server");
+  });
+
+// Assume that if a qurest contains data it is encoded as JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Decode request data
+app.use(bodyParser.json());
+
+app.listen(port);
+
+console.log(`Server is listening on port ${port}`);
+
+export default app;
