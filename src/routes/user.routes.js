a1d89ec1cf9ffe8767356331e18245358f6f27a7
Hamilton Chevez <hchev001@fiu.edu>
2018-09-26 11:33:02 -0400

    added the routes and binded them to controllers

---
+++ src/routes/user.routes.js
@@ -0,0 +1,14 @@
+import UserController from "../controllers";
+
+export default app => {
+  app
+    .route("/users")
+    .get(UserController.list_all_users)
+    .post(UserController.create_a_user);
+
+  app
+    .route("/users/:id")
+    .get(UserController.read_a_user)
+    .put(UserController.update_a_user)
+    .delete(UserController.delete_a_user);
+};
