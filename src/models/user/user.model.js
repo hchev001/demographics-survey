4822130db8fe1f69e2bf9b03feae15b63c27426e
Hamilton Chevez <hchev001@fiu.edu>
2018-09-26 11:20:04 -0400

    working on adding bcrypt to the project for authentication

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -1,6 +1,9 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
+const SALT_WORK_FACTOR = 10;
+
+const User = new Schema({
   createdAt: {
     type: Date,
     default: Date.now
@@ -21,4 +24,31 @@ const userSchema = new Schema({
   }
 });
 
-export default new mongoose.model("User", userSchema);
+User.pre("save", function(next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or if is new)
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+User.methods.comparePassword = (candidatePassword, cb) => {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+export default new mongoose.model("User", User);
