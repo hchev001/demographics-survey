1769b6f2c06effadba9f260bf1acc684dcd2b5f4
Hamilton Chevez <hchev001@fiu.edu>
2018-09-29 23:52:25 -0400

    while trying to figure out how to delete an object from mongoose query result, I learned that it is immutable and to produce a plain js object that is editable I must invoke the lean() function on the query result first

--- src/controllers/authentication/auth.controller.js
+++ src/controllers/authentication/auth.controller.js
@@ -57,47 +57,50 @@ export const sign_up = (req, res, next) => {
  */
 export const log_in = (req, res, next) => {
   // find the user by their username
-  User.findOne({ username: req.body.username }, { __v: 0 }, (err, dbData) => {
-    if (err) {
-      res.status(502).send({});
-    } else if (typeof dbData === "undefined" || dbData === null) {
-      res.status(404).send({});
-    } else {
-      // compare hash with provided password
-      bcrypt.compare(req.body.password, dbData.password, (err, match) => {
-        if (err) {
-          res.status(500).send({}); // TODO: log to some global express log
-        }
+  User.findOne({ username: req.body.username }, { __v: 0 })
+    .lean()
+    .exec((err, dbData) => {
+      if (err) {
+        res.status(502).send({});
+      } else if (typeof dbData === "undefined" || dbData === null) {
+        res.status(404).send({});
+      } else {
+        // compare hash with provided password
+        bcrypt.compare(req.body.password, dbData.password, (err, match) => {
+          if (err) {
+            res.status(500).send({}); // TODO: log to some global express log
+          }
 
-        if (match) {
-          const issuer = "DemographicsAPI";
-          const subject = "demo";
-          const audience = "students";
+          if (match) {
+            const issuer = "DemographicsAPI";
+            const subject = "demo";
+            const audience = "students";
 
-          // signing options
-          const signOptions = {
-            issuer,
-            subject,
-            audience,
-            expiresIn: "12h"
-          };
+            // signing options
+            const signOptions = {
+              issuer,
+              subject,
+              audience,
+              expiresIn: "12h"
+            };
 
-          const token = jwt.sign(
-            { id: dbData._id, username: dbData.username, role: dbData.role },
-            config.get("secret"),
-            signOptions
-          );
+            const token = jwt.sign(
+              { id: dbData._id, username: dbData.username, role: dbData.role },
+              config.get("secret"),
+              signOptions
+            );
 
-          // remove the password field
+            // remove the password field
+            delete dbData.password;
 
-          res
-            .status(200)
-            .json({ code: 200, data: dbData, message: "", token: token });
-        } else {
-          // invalid user data for logging in
-          res.status(403).json({});
-        }
-      });
-    }
-  });
+            res
+              .status(200)
+              .json({ code: 200, data: dbData, message: "", token: token });
+          } else {
+            // invalid user data for logging in
+            res.status(403).json({});
+          }
+        });
+      }
+    });
 };
